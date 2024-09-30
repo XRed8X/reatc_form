d7f49a4fe6c2e9a5602fa0afe59bd211aa85c2a6
FJavier <fjavierme818@gmail.com>
2024-09-30 15:43:28 -0600

    Added 10 forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import {FormsComponent, FormUno, FormDos, FormTres, FormCuatro, FormCinco, FormSeis, FormSiete, FormOcho, FormNueve, FormDiez} from './components/formularios'
 
 function App() {
+  const [data, setData] = useState({})
+
+  const onChange = (e) => {
+    e.preventDefault();
+    // console.log(e)
+    const obj = data;
+    obj[e.target.name] = e.target.value;
+    setData(obj)
+    console.log(data)
+
+  }
+
+  const onClick = () => {
+    console.log(data)
+  }
+
+  /* Aqui guardo todos los chingados formularios con una clave y un valor */
+  const comp_forms = {
+
+    '0. Mayo Bolsa de Trabajo': <FormsComponent  onChange={onChange} onClick={onClick}/>,
+    '1. Mayo Tecnologia y Sociedad': <FormUno onChange={onChange} onClick={onClick}/> ,
+    '2. Mayo Ciencia y Medio Ambiente': <FormDos onChange={onChange} onClick={onClick}/> ,
+    '3. Mayo Historia y Política': <FormTres onChange={onChange} onClick={onClick}/> ,
+    '4. Mayo Economía y Finanzas': <FormCuatro onChange={onChange} onClick={onClick}/> ,
+    '5. Mayo Cultura y Sociedad': <FormCinco onChange={onChange} onClick={onClick}/> ,
+    '6. Mayo Ciencia y Tecnología': <FormSeis onChange={onChange} onClick={onClick}/>,
+    '7. Mayo Psicología y Salud Mental': <FormSiete onChange={onChange} onClick={onClick}/> ,
+    '8. Mayo Filosofía y Ética': <FormOcho onChange={onChange} onClick={onClick}/>,
+    '9. Mayo Educación y Aprendizaje': <FormNueve onChange={onChange} onClick={onClick}/> ,
+    '10. Mayo Deportes y Salud': <FormDiez onChange={onChange} onClick={onClick}/>,
+
+  }
+
+  /* Esta cosa de aqui toma los objetos de "comp_forms" y los convierte en un chingao arreglo con una clave y un valor
+  y luego lo mapea para que la chingadera se pueda mostrar en pantalla */
+  const showCards = Object.entries(comp_forms).map(([key, form]) => {
+    return (
+      <Col xs={12} sm={6} lg={4} className="mb-4">
+        <Card className="mt-3" key={key}>
+          <Card.Body className='d-flex flex-column justify-content-between'>
+            <Card.Title className='text-center'>{key}</Card.Title>
+
+            {form}
+
+          </Card.Body>
+        </Card>
+      </Col>
+    )
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Container className="mt-3">
+      <h1 className='text-center'>Bolsa de Mayo de Trabajo</h1>
+    <Row>
+        {/* Toma lo que hay dentro de "showCards" y lo pone aqui */}
+        {showCards}
+      </Row>
+    </Container>
+    </>
+    
+  )
+  
 }
 
 export default App;
